f31fd2980bfc3c9995012ae6eb7218be780d3766
Dan Rye <dan@ryedesigns.com>
2014-05-16 11:23:18 -0400

    First cut at lineman conversion

---
+++ app/js/custom.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+	$( ".group" ).hide();
+	$( ".show" ).click(function() {
+		$( ".group" ).hide();
+		index = $( this ).parent().attr('class');
+		$( this ).parent().addClass("active");
+		$( ".group."+index ).show();
+	});
+});
