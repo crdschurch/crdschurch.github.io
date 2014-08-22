c5ae524ae019d37f26ca7ae165579836616b5bb0
IngageMaddoxT <tony.maddox@ingagepartners.com>
2014-08-22 11:24:11 -0400

    cleaned up gulp sass task

--- config/tasks/sass.js
+++ config/tasks/sass.js
@@ -1,5 +1,4 @@
 var sass = require('gulp-ruby-sass');
-var gulpsass = require('gulp-sass');
 var concat = require('gulp-concat');
 var util = require('gulp-util');
 var autoprefixer = require('gulp-autoprefixer')
@@ -7,8 +6,8 @@ var autoprefixer = require('gulp-autoprefixer')
 module.exports = function(gulp) {
   return gulp.task('sass', function() {
     return gulp.src("sass/main.scss")
-      .pipe(sass({sourcemap: false}).on("error", util.log))
-      .pipe(autoprefixer("last 2 versions", "Firefox >= 20", { cascade: true }))
-      .pipe(gulp.dest("stylesheets"))
+      .pipe(sass({sourcemap: true}).on("error", util.log))
+      .pipe(autoprefixer('last 2 versions', 'Firefox >= 20', { cascade: true }))
+      .pipe(gulp.dest('stylesheets'))
   });
 };
