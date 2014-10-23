c4c0e435ed3153819ab95b7cdee64173edee2242
Tony Maddox <tony.maddox@ingagepartners.com>
2014-10-23 15:16:58 -0400

    removed imagemin work, testing build on heroku

--- config/tasks/imagemin.js
+++ config/tasks/imagemin.js
@@ -2,16 +2,6 @@ var pngcrush = require('imagemin-pngcrush');
 
 module.exports = function(gulp, $) {
   return gulp.task('imagemin', ['jb'], function() {
-    return gulp.src('public/media/*')
-      .pipe($.imagemin({
-        progressive: true,
-        svgoPlugins: [
-          {
-            removeViewBox: false
-          }
-        ],
-        use: [pngcrush()]
-      }))
-      .pipe(gulp.dest('public/media/'));
+    
   });
 };
